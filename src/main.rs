b6cfdef59d6e7de2e48eed146cb14c310d7b39cd
Jake Drew <25826535+jakedrew@users.noreply.github.com>
2026-08-15 22:04:12 +0100

    Adding axum sse to allow a json endpoint at /stream so this can be linked to map later, also switching grpc to start emitting the resonses by default (removing requirement for the request) - basically so the json stream can work.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod sse;
+
+use tokio::sync::broadcast;
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{transport::Server, Request, Response, Status};
 
@@ -8,8 +11,16 @@ pub mod proto {
     tonic::include_proto!("telemetry");
 }
 
-#[derive(Debug, Default)]
-pub struct TelemetryService {}
+pub struct TelemetryService {
+    tx: broadcast::Sender<TelemetryResponse>,
+}
+
+/* 
+ * TODO:
+ *  - Randomise location start points (within radius N of some point)
+ *  - Move point within this area
+ *  - Spawn between 5 - 50 devices for map
+ */                
 
 #[tonic::async_trait]
 impl Telemetry for TelemetryService {
@@ -21,45 +32,80 @@ impl Telemetry for TelemetryService {
     ) -> Result<Response<Self::TelemetryStream>, Status> {
         let _req = request.into_inner();
         let (tx, rx) = tokio::sync::mpsc::channel(16);
+        let mut bus = self.tx.subscribe();
 
         tokio::spawn(async move {
-            let mut seq = 0u64;
             loop {
-                /* 
-                 * TODO:
-                 *  - Randomise location start points (within radius N of some point)
-                 *  - Move point within this area
-                 *  - Spawn between 5 - 50 devices for map
-                 */                
-
-                let reply: TelemetryResponse = TelemetryResponse {
-                    id: String::from("SIM-01"),
-                    sequence_number: seq,
-                    latitude: 3.0,
-                    longitude: 4.0,
-                    altitude: 5.0,
-                };
-
-                if tx.send(Ok(reply)).await.is_err() {
-                    break;
+                match bus.recv().await {
+                    Ok(message) => {
+                        if tx.send(Ok(message)).await.is_err() {
+                            break;
+                        }
+                    }
+                    Err(broadcast::error::RecvError::Lagged(_n)) => { continue; }
+                    Err(broadcast::error::RecvError::Closed) => break,
                 }
-
-                seq += 1;
-
-                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
             }
         });
-
         Ok(Response::new(ReceiverStream::new(rx)))
+    }
+}
+
+async fn generate_device(device: String, tx: broadcast::Sender<TelemetryResponse>) {
+    let mut tick = tokio::time::interval(std::time::Duration::from_secs(1));
+    let mut seq = 0u64;
+
+    loop {
+        tick.tick().await;
+
+        let reply = TelemetryResponse {
+            id: device.clone(),
+            sequence_number: seq,
+            latitude: 3.0,
+            longitude: 4.0,
+            altitude: 5.0,
+        };
+
+        let _ = tx.send(reply);
+        seq += 1;
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:50051".parse()?;
+    let (app, sse_tx) = sse::router();
+    let (tx, _) = broadcast::channel::<TelemetryResponse>(256);
+
+    for n in 1..=5 {
+        let device = format!("SIM-{n}");
+        tokio::spawn(generate_device(device, tx.clone()));
+    }
+
+    let mut rx = tx.subscribe();
+    tokio::spawn(async move {
+        while let Ok(message) = rx.recv().await {
+
+            let _ = sse_tx.send(
+                serde_json::json!({
+                    "id": message.id,
+                    "sequence_number": message.sequence_number,
+                    "latitude": message.latitude,
+                    "longitude": message.longitude,
+                    "altitude": message.altitude,
+                }).to_string(),
+            );
+        }
+    });
+
+    tokio::spawn(async {
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+        axum::serve(listener, app).await.unwrap();
+    });
+
+    let address = "0.0.0.0:50051".parse()?;
     Server::builder()
-        .add_service(TelemetryServiceServer::new(TelemetryService::default()))
-        .serve(addr)
+        .add_service(TelemetryServiceServer::new(TelemetryService { tx }))
+        .serve(address)
         .await?;
 
     Ok(())
---
+++ src/sse.rs
@@ -0,0 +1,32 @@
+use axum::extract::State;
+use axum::response::sse::{Event, KeepAlive, Sse};
+use axum::routing::get;
+use axum::Router;
+use std::convert::Infallible;
+use tokio_stream::wrappers::BroadcastStream;
+use tokio_stream::StreamExt;
+
+pub type Tx = tokio::sync::broadcast::Sender<String>;
+
+pub fn router() -> (Router, Tx) {
+    let (tx, _rx) = tokio::sync::broadcast::channel::<String>(256);
+
+    let router = Router::new()
+        .route("/stream", get(stream_handler))
+        .with_state(tx.clone());
+
+    return (router, tx);
+}
+
+async fn stream_handler(State(tx): State<Tx>,) -> Sse<impl tokio_stream::Stream<Item = Result<Event, Infallible>>> {
+    let rx = tx.subscribe();
+
+    let stream = BroadcastStream::new(rx).filter_map(|message| {
+        match message {
+            Ok(json) => Some(Ok(Event::default().data(json))),
+            Err(_) => None,
+        }
+    });
+
+    return Sse::new(stream).keep_alive(KeepAlive::default());
+}
